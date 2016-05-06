7f07f5f4845e093fb8a7ab1b74f0e4c336fb873f
Vaibhav-Mittal <vaibhav.mittal@avasyu.com>
2016-05-06 14:08:00 +0530

    two

--- hello/settings.py
+++ hello/settings.py
@@ -115,13 +115,13 @@ USE_L10N = True
 
 USE_TZ = True
 
-
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
-STATIC_ROOT = 'staticfiles'
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'world/static'),
+    os.path.join(PROJECT_ROOT, 'static'),
 )
